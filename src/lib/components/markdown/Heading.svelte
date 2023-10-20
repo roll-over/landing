6b7620fe0c07ad83cdf75a5a093b6fd962667d16
Timur Bondarenko <tibode495@gmail.com>
2023-10-20 18:17:51 +0600

    fixing for mobile

--- src/lib/components/markdown/Heading.svelte
+++ src/lib/components/markdown/Heading.svelte
@@ -4,11 +4,9 @@
 </script>
 
 {#if depth === 1}
-  <h3 class="text-2xl font-semibold">{text}</h3>
+  <h3 class="text-xl font-semibold">{text}</h3>
 {:else if depth === 2}
-  <h4 class="text-xl font-semibold">{text}</h4>
-{:else if depth === 3}
-  <h5 class="text-lg font-semibold">{text}</h5>
+  <h4 class="text-lg font-semibold">{text}</h4>
 {:else}
-  <h6 class="text-ms font-semibold">{text}</h6>
+  <h5 class="font-semibold">{text}</h5>
 {/if}
