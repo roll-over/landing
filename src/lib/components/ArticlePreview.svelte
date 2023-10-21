d7acc656100bd4c280b11057f7d6f54082cf9610
Timur Bondarenko <tibode495@gmail.com>
2023-10-21 15:37:51 +0600

    fix covering text

--- src/lib/components/ArticlePreview.svelte
+++ src/lib/components/ArticlePreview.svelte
@@ -19,9 +19,15 @@
     >
   {:else if article.type === "youtube"}
     <div class="flex flex-col gap-10">
-      <div class="max-h-56 sm:h-36 overflow-hidden">
+      <div class="max-h-56 sm:h-36 overflow-hidden article">
         <OnePost source={article.description} />
+        <div class="frosted" />
       </div>
+      <a
+        href={link}
+        class="text-teal-400 hover:text-teal-600 border-2 border-cyan-900 hover:border-cyan-800 hover:underline underline-offset-1 rounded-xl p-2"
+        >Подробнее</a
+      >
 
       <iframe
         height="315"
@@ -32,10 +38,40 @@
         allowfullscreen
       />
     </div>
-    <a
-      href={link}
-      class="text-teal-400 hover:text-teal-600 border-2 border-cyan-900 hover:border-cyan-800 hover:underline underline-offset-1 rounded-xl p-2"
-      >Подробнее</a
-    >
   {/if}
 </section>
+
+<svg xmlns="http://www.w3.org/2000/svg" version="1.1">
+  <defs>
+    <filter id="blur">
+      <feGaussianBlur stdDeviation="5" />
+    </filter>
+  </defs>
+</svg>
+
+<style>
+  .article {
+    position: relative;
+  }
+  /* The content */
+  .frosted::before {
+    filter: url("#blur");
+    background-color: black;
+    -webkit-filter: blur(8px);
+    -moz-filter: blur(8px);
+    -ms-filter: blur(8px);
+    -o-filter: blur(8px);
+    filter: url("#blur");
+    z-index: 2;
+    background-size: cover;
+  }
+  .frosted {
+    position: absolute;
+    bottom: -10px;
+    width: 100%;
+    height: 20px;
+    filter: url("#blur");
+    background-color: rgba(0, 0, 0, 0.479);
+    background-size: cover;
+  }
+</style>
