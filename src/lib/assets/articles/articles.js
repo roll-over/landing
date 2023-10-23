999e2b73967376571564fa03e1f0d4958a1b0707
Timur Bondarenko <tibode495@gmail.com>
2023-10-23 16:41:45 +0600

    add new article

--- src/lib/assets/articles/articles.js
+++ src/lib/assets/articles/articles.js
@@ -1,11 +1,17 @@
 import first from "./red-flags/employee/check-cv/pochemu_moye_rezyume_ne_parsitsya_sekrety_uspekhnoy_integratsii_v_crm.md?raw";
 import management_in_the_post_covid_era_Brainstorm_its_open_mic from "./its-open-mic/management_in_the_post_covid_era_Brainstorm_its_open_mic.md?raw";
 import pochemu_senyory_ne_stanovyatsya_lidami from "./its-open-mic/pochemu_senyory_ne_stanovyatsya_lidami.md?raw";
+import kakie_vyrazheniya_ne_stoit_ispolzovat_v_rezyume from "./red-flags/employee/check-cv/kakie_vyrazheniya_ne_stoit_ispolzovat_v_rezyume.md?raw";
 const redFlags = {
   employee: {
     cv: {
       link: "/projects/red-flags/employee/check-cv/posts/",
       articles: [
+        {
+          id: "kakie_vyrazheniya_ne_stoit_ispolzovat_v_rezyume",
+          source: kakie_vyrazheniya_ne_stoit_ispolzovat_v_rezyume,
+          type: "md",
+        },
         {
           id: "pochemu_moye_rezyume_ne_parsitsya_sekrety_uspekhnoy_integratsii_v_crm",
           source: first,
