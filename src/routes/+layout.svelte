f7763fcafa74a62e364e3689cee2c341c3c0ae31
Timur Bondarenko <tibode495@gmail.com>
2023-10-27 12:47:48 +0600

    fix double scroll

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -80,57 +80,53 @@
   };
 </script>
 
-<div class="flex flex-col h-screen overflow-hidden">
-  <nav class="p-2 flex flex-col justify-between max-h-40" bind:clientWidth={w}>
-    <div class="p-2 flex justify-between max-h-20">
-      <a href="/" class="flex flex-col">
-        <div class="flex">
-          <img src={Logo} alt="logo" class="h-10" />
-          <img src={Header} alt="roll-over" class="h-10" />
-        </div>
-        <p class="text-slate-400">Группа открытых проектов</p>
-      </a>
-
-      <div
-        class="flex flex-col sm:flex-row gap-5 p-3 text-xl h-fit rounded-2xl"
-      >
-        {#if w < 640}
-          <button
-            class="text-teal-400"
-            on:click={() => {
-              visible = !visible;
-            }}
-          >
-            Меню
-          </button>
-        {/if}
-        {#if w > 640 || visible}
-          <ul
-            class="flex flex-col sm:flex-row gap-3 bg-gray-950 z-10"
-            on:click={() => (visible = false)}
-          >
-            {#each links as link}
-              <li>
-                <Link href={link.href}>{link.title}</Link>
-              </li>
-            {/each}
-          </ul>
-        {/if}
+<nav class="p-2 flex flex-col justify-between max-h-40" bind:clientWidth={w}>
+  <div class="p-2 flex justify-between max-h-20">
+    <a href="/" class="flex flex-col">
+      <div class="flex">
+        <img src={Logo} alt="logo" class="h-10" />
+        <img src={Header} alt="roll-over" class="h-10" />
       </div>
-    </div>
-    <div class="pl-5">
-      {#each steps as step, i}
-        <a href={getUrl(steps, step)}
-          >{getTitle(stepsWithLinksAndTitles[step]?.title || step)}</a
+      <p class="text-slate-400">Группа открытых проектов</p>
+    </a>
+
+    <div class="flex flex-col sm:flex-row gap-5 p-3 text-xl h-fit rounded-2xl">
+      {#if w < 640}
+        <button
+          class="text-teal-400"
+          on:click={() => {
+            visible = !visible;
+          }}
         >
-        {#if step !== steps[steps.length - 1]} {" > "} {/if}
-      {/each}
+          Меню
+        </button>
+      {/if}
+      {#if w > 640 || visible}
+        <ul
+          class="flex flex-col sm:flex-row gap-3 bg-gray-950 z-10"
+          on:click={() => (visible = false)}
+        >
+          {#each links as link}
+            <li>
+              <Link href={link.href}>{link.title}</Link>
+            </li>
+          {/each}
+        </ul>
+      {/if}
     </div>
-  </nav>
-  <main
-    class="flex flex-col items-center overflow-auto"
-    on:click={(e) => (visible = false)}
-  >
-    <slot />
-  </main>
-</div>
+  </div>
+  <div class="pl-5">
+    {#each steps as step, i}
+      <a href={getUrl(steps, step)}
+        >{getTitle(stepsWithLinksAndTitles[step]?.title || step)}</a
+      >
+      {#if step !== steps[steps.length - 1]} {" > "} {/if}
+    {/each}
+  </div>
+</nav>
+<main
+  class="flex flex-col items-center overflow-auto"
+  on:click={(e) => (visible = false)}
+>
+  <slot />
+</main>
