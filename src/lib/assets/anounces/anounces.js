78b069a6c38127710423768c6e7d0544ae460bc1
Timur Bondarenko <tibode495@gmail.com>
2023-10-27 12:47:37 +0600

    clear anounces

--- src/lib/assets/anounces/anounces.js
+++ src/lib/assets/anounces/anounces.js
@@ -1,7 +1,7 @@
 export const anounces = [
-  {
-    project: "its-open-mic",
-    date: new Date(2023, 9, 26, 17),
-    title: "Митап: Как айтишнику сделать свою рок группу",
-  },
+  // {
+  //   project: "its-open-mic",
+  //   date: new Date(2023, 9, 26, 17),
+  //   title: "Митап: Как айтишнику сделать свою рок группу",
+  // },
 ];
