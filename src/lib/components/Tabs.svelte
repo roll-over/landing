a1da3f0d6d7e41a187b7a2c0d792c27b85cf2e23
Oleg Kokoshinskii <oleg.kokoshinskii@gmail.com>
2023-11-02 19:30:22 +0600

    [contributors-page]: add link target

--- src/lib/components/Tabs.svelte
+++ src/lib/components/Tabs.svelte
@@ -55,7 +55,7 @@
   <ul class="flex justify-center flex-wrap sm:flex-row gap-3">
     {#each activeTabContent as content}
     <li class="w-60 flex flex-col justify-between bg-slate-800  p-1 rounded-2xl">
-      <a class="flex flex-col" href={content.html_url}>
+      <a class="flex flex-col" href={content.html_url} target="_blank">
         <h3 class="flex flex-col space-around h-36 text-2xl font-bold bg-slate-700 hover:bg-slate-600 text-slate-500 py-2 px-4 rounded-xl">
           {content.login}
           <img src={content.avatar_url} alt={`${content.login}'s avatar'`} class='w-14 mt-auto rounded-full' />
