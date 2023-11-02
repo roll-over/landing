aedcf188527318754375d37c2edccb3b613c2912
Oleg Kokoshinskii <oleg.kokoshinskii@gmail.com>
2023-11-02 19:05:59 +0600

    [contributors-page]: add cards

--- src/lib/components/Tabs.svelte
+++ src/lib/components/Tabs.svelte
@@ -45,16 +45,30 @@
       class="tab {index === activeTab ? 'active' : ''}"
       on:click={() => switchTab(index)}
     >
-      {tab.name}
+      {tab.name}      
     </button>
   {/each}
 </div>
 
 <div class="tab-content">
   {#if tabs[activeTab] && activeTabContent}
-      {#each activeTabContent as content}
-        <p>{content.login}</p>
-      {/each}
+  <ul class="flex justify-center flex-wrap sm:flex-row gap-3">
+    {#each activeTabContent as content}
+    <li class="w-60 flex flex-col justify-between bg-slate-800 p-1 rounded-2xl">
+      <a class="flex flex-col" href={content.html_url}>
+        <h3 class="flex flex-col space-around h-36 text-2xl font-bold bg-slate-700 text-slate-500 py-2 px-4 rounded-xl">
+          {content.login}
+          <img src={content.avatar_url} alt={`${content.login}'s avatar'`} class='w-14 mt-auto rounded-full' />
+        </h3>
+
+        <p class="text-slate-500 font-bold bg-slate-800 flex justify-end p-2">
+          Contributions: {content.contributions}
+        </p>
+      </a>
+      <div>
+    </li>
+    {/each}
+  </ul>
   {:else}
     Loading.render..
   {/if}
