c2c5f084ba9cc58fc5bbfa7d8d831684402813bf
Timur Bondarenko <tibode495@gmail.com>
2023-11-04 13:45:25 +0600

    add trustHost

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,8 @@
-import { SvelteKitAuth } from "@auth/sveltekit"
-import Google from "@auth/core/providers/google"
-import { GOOGLE_ID, GOOGLE_SECRET } from "$env/static/private"
+import { SvelteKitAuth } from "@auth/sveltekit";
+import Google from "@auth/core/providers/google";
+import { GOOGLE_ID, GOOGLE_SECRET } from "$env/static/private";
 
 export const handle = SvelteKitAuth({
+  trustHost: true,
   providers: [Google({ clientId: GOOGLE_ID, clientSecret: GOOGLE_SECRET })],
-})
+});
