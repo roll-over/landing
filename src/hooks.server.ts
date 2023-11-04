7ec114bf3702f38f38cb55b2c93632729c6e08dc
Timur Bondarenko <tibode495@gmail.com>
2023-11-04 13:13:12 +0600

    set staic private

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,6 +1,6 @@
 import { SvelteKitAuth } from "@auth/sveltekit"
 import Google from "@auth/core/providers/google"
-import { GOOGLE_ID, GOOGLE_SECRET } from "$env/dynamic/private"
+import { GOOGLE_ID, GOOGLE_SECRET } from "$env/static/private"
 
 export const handle = SvelteKitAuth({
   providers: [Google({ clientId: GOOGLE_ID, clientSecret: GOOGLE_SECRET })],
