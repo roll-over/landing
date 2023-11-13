24c6c961cda7c640b9c8e515987d4b1a7d29eede
Timur Bondarenko <tibode495@gmail.com>
2023-11-13 21:22:13 +0600

    fix cors

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -10,17 +10,15 @@ const svelteHandle = SvelteKitAuth({
 });
 
 export const handle: Handle = async ({ resolve, event }) => {
+
+  const response = await svelteHandle({ resolve, event });
+
   if (event.url.pathname.startsWith("/embed")) {
-    return new Response(null, {
-      headers: {
-        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Headers": "*",
-      },
-    });
+    response.headers.append('Access-Control-Allow-Origin', `*`);
+
   }
 
-  return svelteHandle({ resolve, event });
+  return response
 };
 
 startMongo()
--- src/routes/embed/widget.js
+++ src/routes/embed/widget.js
@@ -212,3 +212,4 @@ widget.appendChild(widgetFooter);
 
 document.body.appendChild(widgetButton);
 document.body.appendChild(widget);
+console.log("widget loaded");
