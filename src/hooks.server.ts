e1ccc2d6c77e633c67d9180215e2aae1373c1fd1
Timur Bondarenko <tibode495@gmail.com>
2023-11-13 21:27:46 +0600

    fix headers for options

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -10,15 +10,24 @@ const svelteHandle = SvelteKitAuth({
 });
 
 export const handle: Handle = async ({ resolve, event }) => {
-
+  if (event.url.pathname.startsWith("/embed")) {
+    if (event.request.method === "OPTIONS") {
+      return new Response(null, {
+        headers: {
+          "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Headers": "*",
+        },
+      });
+    }
+  }
   const response = await svelteHandle({ resolve, event });
 
   if (event.url.pathname.startsWith("/embed")) {
-    response.headers.append('Access-Control-Allow-Origin', `*`);
-
+    response.headers.append("Access-Control-Allow-Origin", `*`);
   }
 
-  return response
+  return response;
 };
 
 startMongo()
