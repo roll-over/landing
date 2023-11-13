f0f6ef6d96f6db3baa636e9ebb5b566052bca796
Timur Bondarenko <tibode495@gmail.com>
2023-11-13 21:14:28 +0600

    add cors for embed

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,11 +2,26 @@ import { SvelteKitAuth } from "@auth/sveltekit";
 import Google from "@auth/core/providers/google";
 import { GOOGLE_ID, GOOGLE_SECRET } from "$env/static/private";
 import { startMongo } from "$lib/db";
+import type { Handle } from "@sveltejs/kit";
 
-export const handle = SvelteKitAuth({
+const svelteHandle = SvelteKitAuth({
   trustHost: true,
   providers: [Google({ clientId: GOOGLE_ID, clientSecret: GOOGLE_SECRET })],
 });
+
+export const handle: Handle = async ({ resolve, event }) => {
+  if (event.url.pathname.startsWith("/embed")) {
+    return new Response(null, {
+      headers: {
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "*",
+      },
+    });
+  }
+
+  return svelteHandle({ resolve, event });
+};
 
 startMongo()
   .then(() => {
