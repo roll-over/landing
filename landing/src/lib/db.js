fd1255e4a739c27f121a782ebf28f764e3c2d4c7
Timur Bondarenko <tibode495@gmail.com>
2023-11-24 13:57:02 +0600

    add options for mongo

--- landing/src/lib/db.js
+++ landing/src/lib/db.js
@@ -3,7 +3,10 @@ import { ME_CONFIG_MONGODB_URL } from "$env/static/private";
 
 const client = new MongoClient(
   ME_CONFIG_MONGODB_URL,
-  { useUnifiedTopology: true}
+  { useUnifiedTopology: true },
+  { useNewUrlParser: true },
+  { connectTimeoutMS: 30000 },
+  { keepAlive: 1 },
 );
 
 export const startMongo = async () => {
