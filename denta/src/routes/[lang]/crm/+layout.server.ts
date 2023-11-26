16607bf0439a891377cb1ae540c59a51de76baeb
Timur Bondarenko <tibode495@gmail.com>
2023-11-27 00:56:53 +0600

    fix crm session

--- denta/src/routes/[lang]/crm/+layout.server.ts
+++ denta/src/routes/[lang]/crm/+layout.server.ts
@@ -1,18 +1,19 @@
 import db from "$lib/db";
-import { redirect } from "@sveltejs/kit";
 
 export const load = async (event) => {
   const session = await event.locals.getSession();
 
-  const companies = await db()
-    .collection("companies")
-    .find(
-      {
-        owner: session.user.email,
-      },
-      { projection: { _id: 0 } },
-    )
-    .toArray();
+  const companies = session
+    ? await db()
+        .collection("companies")
+        .find(
+          {
+            owner: session.user.email,
+          },
+          { projection: { _id: 0 } },
+        )
+        .toArray()
+    : [];
 
   return {
     companies: companies,
