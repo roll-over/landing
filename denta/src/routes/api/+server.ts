6553fc35ab1b2bbda644512f25dcf4e40fc81930
Timur Bondarenko <tibode495@gmail.com>
2023-11-29 18:16:51 +0600

    add manual hendler for info companies

---
+++ denta/src/routes/api/+server.ts
@@ -0,0 +1,39 @@
+import db from "$lib/db";
+import { env } from "$env/dynamic/private";
+
+export async function POST(event) {
+  if (event.cookies.get("ADMIN_SESSION") !== env.ADMIN_SESSION) {
+    return new Response(JSON.stringify({}), {
+      status: 401,
+      headers: {
+        "content-type": "application/json",
+      },
+    });
+  }
+  const infoCompanies = await event.request.json();
+
+  infoCompanies.forEach(async (infoCompany) => {
+    await db()
+      .collection("info-companies")
+      .updateOne(
+        {
+          _id: infoCompany._id,
+        },
+        {
+          $set: {
+            ...infoCompany,
+          },
+        },
+        {
+          upsert: true,
+        },
+      );
+  });
+
+  return new Response(JSON.stringify({}), {
+    status: 200,
+    headers: {
+      "content-type": "application/json",
+    },
+  });
+}
