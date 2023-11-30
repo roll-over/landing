fba545f917e3f5d82907fc55727efb5e289588cf
Timur Bondarenko <tibode495@gmail.com>
2023-11-30 17:41:35 +0600

    update sitemaps

--- sitemap-generator/main.py
+++ sitemap-generator/main.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     crawler(
         "https://denta-crm.com/",
         out_file="denta/static/sitemap.xml",
-        exclude_urls=[".pdf", ".jpg", ".zip"],
+        exclude_urls=[".pdf", ".jpg", ".zip", "crm/"],
         http_request_options={"ssl": True},
         parser=Parser,
     )
