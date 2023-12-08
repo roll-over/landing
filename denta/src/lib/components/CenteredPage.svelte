5e0ce44ece291c697511306e00ce6a44d95c0743
Timur Bondarenko <tibode495@gmail.com>
2023-12-08 11:38:37 +0600

    add companies picker placeholders

--- denta/src/lib/components/CenteredPage.svelte
+++ denta/src/lib/components/CenteredPage.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div class="flex flex-col w-full items-center p-3">
-  <div class="max-w-xl">
+  <div class="max-w-2xl">
     {#if title}
       <h1>{title}</h1>
     {/if}
---
+++ denta/src/routes/[lang]/company/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+  import CenteredPage from "$lib/components/CenteredPage.svelte";
+  import Section from "$lib/components/Section.svelte";
+  import SupportLink from "$lib/components/SupportLink.svelte";
+</script>
+
+<CenteredPage>
+  <h1>Не выбрана компания для просмотра</h1>
+  <Section>
+    <h2 class="card-header w-full">Как выбрать компанию?</h2>
+    <p class="p-4 w-full">Пожалуйста, выберите компанию в поиске для просмотра.</p>
+    <footer class="card-footer w-full">
+      <SupportLink href="/ru/search/">Перейти в поиск</SupportLink>
+    </footer>
+  </Section>
+  <Section>
+    <h2 class="card-header w-full">Как создать компанию?</h2>
+    <p class="p-4 w-full">Если у вас нет компании, то создайте её в</p>
+    <footer class="card-footer w-full">
+      <SupportLink href="/ru/crm/create-company">соответствующем разделе</SupportLink>.
+    </footer>
+  </Section>
+</CenteredPage>
