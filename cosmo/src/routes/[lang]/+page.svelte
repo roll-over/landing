bbc62567a8c6fa4990fc2444287297f957ad3d6f
Timur Bondarenko <tibode495@gmail.com>
2024-01-15 01:42:38 +0600

    update cosmostyle

--- cosmo/src/routes/[lang]/+page.svelte
+++ cosmo/src/routes/[lang]/+page.svelte
@@ -22,20 +22,20 @@
   </p>
 </div>
 <ul class="flex w-full flex-col items-center justify-center gap-5 p-2 md:gap-10 md:p-5">
-  <li>
-    <a href={`https://denta-crm.com/${$page.params.lang}?utm_source=cosmo-crm.com`} class="card">
+  <li class="planet_1">
+    <a href={`https://denta-crm.com/${$page.params.lang}?utm_source=cosmo-crm.com`} class="">
       <header class="card-header">
-        <h2 class="">Denta CRM</h2>
+        <h2 class="">🦷 Denta CRM</h2>
       </header>
       <div class="p-4">
         {l("CRM-система для стоматологий")}
       </div>
     </a>
   </li>
-  <li>
-    <a href={`https://femida-crm.com/${$page.params.lang}?utm_source=cosmo-crm.com`} class="card">
+  <li class="planet_2">
+    <a href={`https://femida-crm.com/${$page.params.lang}?utm_source=cosmo-crm.com`} class="">
       <header class="card-header">
-        <h2 class="">Femida CRM</h2>
+        <h2 class="">⚖️ Femida CRM</h2>
       </header>
       <div class="p-4">
         {l("CRM-система для юристов")}
@@ -44,7 +44,7 @@
   </li>
 </ul>
 
-<style>
+<style lang="scss">
   .p {
     width: 100%;
     max-width: 500px;
@@ -52,12 +52,32 @@
 
   li {
     width: 100%;
-    max-width: 500px;
-    background: rgba(255, 255, 255, 0.474);
+    max-width: 450px;
+    height: 120px;
+    background: rgba(26, 129, 129, 0.595);
     border-radius: 16px;
-    box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+    box-shadow: 0 0 30px rgba(0, 0, 0, 0.5);
     backdrop-filter: blur(5px);
     -webkit-backdrop-filter: blur(5px);
     border: 1px solid rgba(255, 255, 255, 0.3);
+
+    transition: all 0.2s ease-in-out;
+    a {
+      width: 100%;
+      height: 100%;
+      display: flex;
+      flex-direction: column;
+      text-decoration: none;
+      color: inherit;
+      text-align: right;
+    }
+    h2{
+      text-align: left;
+    }
+  }
+
+   li:hover {
+    border-width: 3px;
+    box-shadow: 0 0 50px rgba(156, 156, 156, 0.5);
   }
 </style>
