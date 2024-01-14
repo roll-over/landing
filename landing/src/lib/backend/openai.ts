880f98830a5e3f63d25c459259b0d4957921d764
Timur Bondarenko <tibode495@gmail.com>
2024-01-14 15:51:08 +0600

    fix open ai key

--- landing/src/lib/backend/openai.ts
+++ landing/src/lib/backend/openai.ts
@@ -1,6 +1,6 @@
 import OpenAI from "openai";
-import { OPENAI_API_KEY } from "$env/static/private";
+import { env } from "$env/dynamic/private";
 
 export const openai = new OpenAI({
-  apiKey: OPENAI_API_KEY,
+  apiKey: env.OPENAI_API_KEY,
 });
